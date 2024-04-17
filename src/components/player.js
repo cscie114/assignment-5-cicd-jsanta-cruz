f33f3d42e0d4b942b681db48d089dfb75a651a49
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2024-04-16 22:07:58 -0400

    added optional chaining to player component

--- src/components/player.js
+++ src/components/player.js
@@ -4,13 +4,13 @@ import { player_article } from './player.module.css';
 const Player = ({ player }) => {
   return (
     <div className={player_article}>
-        {player.players.map((ply, i) => (
+        {player?.players.map((ply, i) => (
         <article key={i}>
-        <h3>{ply.name}</h3>
-        <img loading="lazy" width={250} height={194} src={ply.image} alt={ply.name} />
-        <p>Number: {ply.number}</p>
-        <p>Position: {ply.position}</p>
-        <p>Group: {ply.group}</p>
+        <h3>{ply?.name}</h3>
+        <img loading="lazy" width={250} height={194} src={ply?.image} alt={ply?.name} />
+        <p>Number: {ply?.number}</p>
+        <p>Position: {ply?.position}</p>
+        <p>Group: {ply?.group}</p>
         </article>
         ))}
     </div>
