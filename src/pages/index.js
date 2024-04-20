1c99719a3e34908e2e67bd684a4c4c35929dbe4d
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2024-04-20 14:43:58 -0400

    post-production build test

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const HomePage = () => {
   return (
-    <Layout pageTitle="The NFL Roster for the 2023 Season">
+    <Layout pageTitle="The NFL Roster for the 2023 Season!">
         <StaticImage src="../images/nfl-hero-sf.webp" height={600} alt="San Francisco 49ers running back Christian McCaffrey (23)"></StaticImage>
     </Layout>
   )
