3b40c0e39cbf617dff73a04f3e3232dc4bd99869
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2024-04-21 01:29:34 -0400

    eslint ignore empty object in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-
-const Footer = ({ data }) => {
+  // eslint-disable-next-line no-empty-pattern
+const Footer = ({ }) => {
     return (
 <footer>
-{data.site.siteMetadata.title} Example footer developed for CSCI E-114, Spring 2024.
+ Example footer developed for CSCI E-114, Spring 2024.
 </footer>
     );
 }
