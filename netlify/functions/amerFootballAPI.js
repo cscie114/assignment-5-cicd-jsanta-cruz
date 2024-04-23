df62915ee4ceb8cb033310bfec5dfdca3e2e301f
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2024-04-22 22:17:31 -0400

    first attempt at serverless function deployment

--- netlify/functions/amerFootballAPI.js
+++ netlify/functions/amerFootballAPI.js
@@ -1,8 +1,7 @@
 const fetch = require('node-fetch');
 require("dotenv").config();
 
-const handler = async function (event, context) {
-    // console.log(event)
+const handler = async function (event) {
     const apiKey = process.env.NFL_API_KEY;
     const { team } = event.queryStringParameters;
     const meta = {
@@ -11,39 +10,29 @@ const handler = async function (event, context) {
         "Content-Type": "application/json"
         };
     const headers = new Headers(meta);
-      const fetchOptions = {
+    const fetchOptions = {
           "headers": headers
       }
     try{
         const response = await fetch(`https://api-american-football.p.rapidapi.com/injuries?team=${team}`, fetchOptions)
+        if (!response.ok) {
+            // NOT res.status >= 200 && res.status < 300
+            return { statusCode: response.status, body: response.statusText }
+        }
         const data = await response.json()
+        console.log(data)
+
         return {
            statusCode: 200,
            body: JSON.stringify({ data }) 
         }
     } catch (error) {
+        console.log(error)
         return {
             statusCode: 500,
             body: JSON.stringify({ msg: error.message })
         }
     }
 
-
 }
 module.exports = {handler}
-
-
-/*
-    const meta = {
-      'X-RapidAPI-Key': apiKey,
-      "X-RapidAPI-Host": "api-american-football.p.rapidapi.com",
-      "Content-Type": "application/json"
-      };
-  const headers = new Headers(meta);
-    const fetchOptions = {
-        "headers": headers
-    }
-let responseData = {}
-        let response = await fetch(requestUrl, fetchOptions);
-        responseData = await response.json();
-        */
--- src/pages/injury-report.js
+++ src/pages/injury-report.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../components/layout";
 import Footer from "../components/footer";
+import * as playerStyles from "../components/player.module.css";
 import fetch from "node-fetch";
 
 const InjuryReport = () => {
@@ -8,30 +9,30 @@ const InjuryReport = () => {
   useEffect(() => {
     fetch("/.netlify/functions/amerFootballAPI?team=1", {
       method: "GET",
-    })
-      .then((result) => {
+    }).then((result) => {
+        console.log("injury data 1", result)
+
         return result.json();
-      })
-      .then((json) => {
+      }).then((json) => {
         console.log("injury: ", json.data)
         setInjuryData(json.data.response);
       });
-  }, [injuryData.id]);
-
+  }, [injuryData.player]);
+console.log("injury data", injuryData)
   return (
     <div>
       <Layout pageTitle="Injury Report for the 2023 Season">
-        <ul>
-          {injuryData.map((injury, index) => {
-            return (
-              <li key={index}>
-                <div>
-                  {injury?.player?.name} - {injury?.description}
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        <div className={playerStyles.player_article}>
+          {Object.entries(injuryData).map((item, i) => {
+                return <article key={i}>
+                <h3>{item[1].player.name}</h3>
+                <img loading="lazy" width={250} height={194} src={item[1].player.image} alt={item[1].player.name} />
+                <p>Injury: {item[1].description}</p>
+                <p>Status: {item[1].status}</p>
+                <p>Team: {item[1].team.name}</p>
+                </article>
+            })}
+        </div>
       </Layout>
       <Footer />
     </div>
